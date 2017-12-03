8dc1592ec0f88b39144395aae626e0fce66f21c2
Robert Haines <rhaines@manchester.ac.uk>
2017-12-03 13:55:01 +0000

    Add library function for day 1 solution.

---
+++ lib/aoc-2017.rb
@@ -0,0 +1,15 @@
+#
+# Advent of Code 2017
+#
+# Robert Haines
+#
+# Public Domain
+#
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'reverse-captcha'
+
+module AOC17
+end
---
+++ lib/reverse-captcha.rb
@@ -0,0 +1,25 @@
+#
+# Advent of Code 2017
+#
+# Robert Haines
+#
+# Public Domain
+#
+
+module AOC17
+
+  def reverse_captcha(numbers, distance = 1)
+    length = numbers.length
+    numbers = numbers + numbers[0...distance]
+
+    acc = 0
+    length.times do |i|
+      if numbers[i] == numbers[i + distance]
+        acc += numbers[i]
+      end
+    end
+
+    acc
+  end
+
+end
