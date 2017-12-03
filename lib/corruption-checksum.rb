1e626c24874b56de327efe06266a7d95b8f6cafd
Robert Haines <rhaines@manchester.ac.uk>
2017-12-03 16:06:47 +0000

    Refactor checksum to use other calculations.

--- lib/corruption-checksum.rb
+++ lib/corruption-checksum.rb
@@ -8,10 +8,14 @@
 
 module AOC17
 
-  def corruption_checksum(spreadsheet)
+  def corruption_checksum(spreadsheet, method = :diff)
     spreadsheet.reduce(0) do |sum, row|
-      sum += (row.max - row.min)
+      sum += send("row_" + method.to_s, row)
     end
+  end
+
+  def row_diff(row)
+    row.max - row.min
   end
 
 end
