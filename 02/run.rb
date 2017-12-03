58946719cd16894078f41f87c772f8b6eb1f16c3
Robert Haines <rhaines@manchester.ac.uk>
2017-12-03 15:25:01 +0000

    Solve day 2, part 1.

---
+++ 02/run.rb
@@ -0,0 +1,20 @@
+#
+# Advent of Code 2017
+#
+# Robert Haines
+#
+# Public Domain
+#
+
+require 'aoc-2017'
+
+include AOC17
+
+INPUT = File.join(File.dirname(__FILE__), 'input.txt')
+
+input = []
+File.foreach(INPUT) do |line|
+  input.push line.split("\t").map { |x| x.to_i }
+end
+
+puts "Part 1: #{corruption_checksum(input)}"
