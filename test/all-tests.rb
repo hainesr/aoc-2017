a4427db67a747e7b0584666b179aa1c5bb7de2ef
Robert Haines <rhaines@manchester.ac.uk>
2017-12-10 23:08:38 +0000

    Round out tests for day 10, part 2.

--- test/all-tests.rb
+++ test/all-tests.rb
@@ -237,6 +237,15 @@ c inc -20 if c == 10"
   def test_full_knot_hash
     knot = KnotHash.new
     assert_equal(knot.full_hash, "a2582a3a0e66e6e86e3812dcb672a272")
+
+    knot = KnotHash.new
+    assert_equal(knot.full_hash("AoC 2017"), "33efeb34ea91902bb2f59c9920caa6cd")
+
+    knot = KnotHash.new
+    assert_equal(knot.full_hash("1,2,3"), "3efbe78a8d82f29979031a4aa0b16a9d")
+
+    knot = KnotHash.new
+    assert_equal(knot.full_hash("1,2,4"), "63960835bcdc130f0b66d7ff4f6a5a8e")
   end
 
 end
