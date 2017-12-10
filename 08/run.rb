ae39dd24365efb8ce48ca247389d9118c4e75b24
Robert Haines <rhaines@manchester.ac.uk>
2017-12-10 15:33:01 +0000

    Solve day 8, part 1.

---
+++ 08/run.rb
@@ -0,0 +1,18 @@
+#
+# Advent of Code 2017
+#
+# Robert Haines
+#
+# Public Domain
+#
+
+require 'aoc-2017'
+
+include AOC17
+
+INPUT = File.join(File.dirname(__FILE__), 'input.txt')
+
+input = File.read(INPUT)
+computer = Computor.process_input(input)
+
+puts "Part 1: #{computer.max_value}"
