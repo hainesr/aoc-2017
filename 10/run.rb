ad63698ff38d6cd5628ec1f8ae201660ffbc2318
Robert Haines <rhaines@manchester.ac.uk>
2017-12-10 23:14:02 +0000

    Solve day 10, part 2.

--- 10/run.rb
+++ 10/run.rb
@@ -12,9 +12,12 @@ include AOC17
 
 INPUT = File.join(File.dirname(__FILE__), 'input.txt')
 
-input = File.read(INPUT).chomp.split(',').map { |c| c.to_i }
+input = File.read(INPUT).chomp
+numbers = input.split(',').map { |c| c.to_i }
+
 knot = KnotHash.new
-
-input.each { |i| knot.k_hash(i) }
-
+numbers.each { |i| knot.k_hash(i) }
 puts "Part 1: #{knot.checksum}"
+
+knot = KnotHash.new
+puts "Part 2: #{knot.full_hash(input)}"
