33063b324cdee453d7990b63b22fe27967a777f3
Robert Haines <rhaines@manchester.ac.uk>
2017-12-27 14:26:16 +0000

    Solve day 22, part 1.

---
+++ 22/run.rb
@@ -0,0 +1,19 @@
+#
+# Advent of Code 2017
+#
+# Robert Haines
+#
+# Public Domain
+#
+
+require 'aoc-2017'
+
+include AOC2017
+
+INPUT = File.join(File.dirname(__FILE__), 'input.txt')
+input = File.read(INPUT).chomp
+
+virus = Sporifica.new(input, 12, 12)
+virus.run(10_000)
+
+puts "Part 1: #{virus.infections}"
